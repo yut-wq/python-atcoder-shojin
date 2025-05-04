0be0825e4ee35f057a8718c9c146dc1ac1b8d23f
yut-wq <println856@gmail.com>
2025-05-04 18:33:20 +0900

    templateに入力とソートのテンプレート追加

--- template.py
+++ template.py
@@ -1,6 +1,9 @@
 def main():
     # n = int(input())
-    # a = input().split()
+    # N, M = map(int, input().split())
+    # B = list(map(int, input().split()))
+    # black.sort(reverse=True)
+
     print("hello world")
 
 
